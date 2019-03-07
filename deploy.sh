de2e085323fc686399a15b3666cf052cc19fe320
julwrites <julian.teh@outlook.com>
2019-03-07 23:34:23 +0800

    Setting -o for curl output

--- deploy.sh
+++ deploy.sh
@@ -52,7 +52,7 @@ function init () {
 function deploy () {
     cd /usr/tmp
 
-    curl -fsSL "https://github.com/julwrites/tools/raw/master/deploy.py" deploy.py
+    curl -fsSL -o deploy.py "https://github.com/julwrites/tools/raw/master/deploy.py"
 
     python deploy.py
 
